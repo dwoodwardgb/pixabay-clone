fc1437ac2ea74e5ef151ddeec0ba27e1541ff856
David Woodward <davidiwoodward@icloud.com>
2021-07-08 19:38:55 -0400

    Demo fetching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = "4272121-59134f1078e603a1211a874d6";
+const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+  "red roses"
+)}`;
+
 function App() {
+  const [x, setX] = useState(false);
+
+  useEffect(() => {
+    if (x) {
+      fetch(URL).then((r) => r.json());
+    }
+  }, [x]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setX(true)}>fetch</button>
     </div>
   );
 }
