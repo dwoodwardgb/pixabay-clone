d990f3683aa2bbbe53a7cb7c2c8df5ed8f264851
David Woodward <davidiwoodward@icloud.com>
2021-07-08 21:49:01 -0400

    Flesh out UI for detail page

--- src/ImageDetail.js
+++ src/ImageDetail.js
@@ -7,8 +7,43 @@ export default function ImageDetail() {
   const [image, setImage] = useState();
 
   useEffect(() => {
-    getImage(imageId).then(setImage);
+    getImage(imageId).then((result) => {
+      setImage(result?.hits?.[0]);
+      console.log(result?.hits?.[0]);
+    });
   }, [setImage, imageId]);
 
-  return <pre>{JSON.stringify(image, null, 2)}</pre>;
+  return (
+    <>
+      {image ? (
+        <section className="flex flex-col items-center my-6">
+          <img
+            alt={image?.tags}
+            src={image?.largeImageURL}
+            className="h-auto w-full"
+            style={{ maxWidth: "850px" }}
+          />
+          <div className="flex flex-row space-x-4">
+            <ul className="space-x-4 flex flex-row items-center flex-wrap">
+              {image?.tags.split(",").map((tag) => (
+                <li key={tag} className="rounded-lg border border-black p-0.5">
+                  {tag}
+                </li>
+              ))}
+            </ul>
+            <div>
+              <img
+                alt={image?.user}
+                src={image?.userImageURL}
+                style={{ width: "100px", height: "100px" }}
+              />
+              {image?.user}
+            </div>
+          </div>
+        </section>
+      ) : (
+        <div>loading...</div>
+      )}
+    </>
+  );
 }
